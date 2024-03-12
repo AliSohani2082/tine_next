968205fd503e951514b047094711aed987fcd7db
AliSohani <a.sohani.shahrood@gmail.com>
2024-03-12 22:55:18 +0330

    fixed some build bugs

--- src/app/(platform)/(auth)/_components/Signup.tsx
+++ src/app/(platform)/(auth)/_components/Signup.tsx
@@ -16,7 +16,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Loader from "@/components/shared/Loader";
+// import Loader from "@/components/shared/Loader";
 import { useToast } from "@/components/ui/use-toast";
 
 // import {
